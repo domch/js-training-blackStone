44397130aede6aa41253421b3ffce43f576481cb
Mehmet Dogan <info@intelligentworks.ch>
2017-11-01 22:22:51 +0100

    reinitiliaze

--- js/blackStone/index.js
+++ js/blackStone/index.js
@@ -1,56 +1,24 @@
-
-$('button').on('click', ()=>{
+$('#initialize').on("click", function(){
     main();
 });
 
 
-const MAX_WIDTH = 420;
+/**
+ * 1) Get user inputs.
+ * 2) Find dimensions.
+ * 3) Create Maze.
+ * 4) Put the black stone in a randomly selected cell.
+ */
 
-function main(){
-    let userInputs = getUserInputs();
-    initMaze();
-    createMaze(userInputs);
-    placeStone(userInputs);
-}
 
-function getUserInputs(){
-    return {
-        row: +$("#mazeRows").val(),
-        column: +$("#mazeCols").val(),
-        color: $("#stoneColor").val()
-    }
-}
+ /**
+  * The entry point of the application
+  */
+ function main(){
+     let inputs = getUserInputs();
+     findDimenions();
+     createMaze();
+     placeBlackStone();
+ }
 
-function initMaze(){
-    $('#maze').html("");
-}
-
-function createMaze({row, column, color}){
-    let dimension = findDimension(column);
-    let content = 
-        `<div style="
-                width:${dimension}px; 
-                height:${dimension}px; 
-                background-color:${color}">
-        </div>`.repeat(row*column);
-    
-    $('#maze').append(content);
-}
-
-function findDimension(column){
-    try{
-        return MAX_WIDTH/column;
-    } catch(e){
-      console.error("Error!");   
-    }
-}
-
-function placeStone({row, column, color}){
-    let stoneCol = Math.floor(Math.random() * column) + 1;
-    let stoneRow = Math.floor(Math.random() * row) + 1;
-    let nthChild = (stoneRow - 1) * column + stoneCol; 
-    
-    $(`#maze div:nth-child(${nthChild})`)
-        .css('background-color', '#000')
-        .html(`${stoneRow}, ${stoneCol}`);
-}
+ 
